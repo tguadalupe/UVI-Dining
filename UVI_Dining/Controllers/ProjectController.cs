b531fe0da6c50e87a6fccec15cad4cea50996c6c
Thalia <thalializbeth97@gmail.com>
2018-11-13 11:01:45 -0800

    Delete files. Added new controller. Added new folder in View. move/create sign up file in Project file

---
+++ UVI_Dining/Controllers/ProjectController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UVI_Dining.Models;
+
+
+namespace UVI_Dining.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //FOR THE PROJECT 
+
+
+        public IActionResult SignUp()
+        {
+            var testing = new MYSQL_CON();
+            testing.admin_login();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignUp(MYSQL_CON model)
+        {
+
+            var testing = new MYSQL_CON()
+            {
+                FName = Request.Form["FName"],
+                LName = Request.Form["LName"],
+                Email = Request.Form["Email"],
+                Password = Request.Form["Password"],
+                user_Status = Request.Form["user_Status"]
+            };
+
+            testing.admin_login();
+
+            return View();
+        }
+    }
+}
--- UVI_Dining/Startup.cs
+++ UVI_Dining/Startup.cs
@@ -56,7 +56,7 @@ namespace UVI_Dining
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Project}/{action=Index}/{id?}");
             });
         }
     }
