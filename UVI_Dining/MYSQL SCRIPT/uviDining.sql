e60935d1f564d89f00befe23a67209ab2f2c92bf
Thalia <thalializbeth97@gmail.com>
2018-11-24 22:03:07 -0800

    UPDATE BIG TIME. MOVE FROM ASP.NET CORE TO ASP.NET FRAMEWORK.

---
+++ UVI_Dining/MYSQL SCRIPT/uviDining.sql
@@ -0,0 +1,62 @@
+DROP DATABASE `uvi_dining`;
+
+create database if not exists UVI_Dining;
+use UVI_Dining;
+
+create table Campus(
+campus_id INT NOT NULL AUTO_INCREMENT,
+campus_loc VARCHAR(45),
+PRIMARY KEY(campus_id));
+
+create table Admins(
+admin_id INT NOT NULL AUTO_INCREMENT,
+FName VARCHAR(45),
+LName VARCHAR(45),
+Email VARCHAR(45),
+Password VARCHAR(45),
+user_Status VARCHAR(45),
+campus_id INT NOT NULL,
+PRIMARY KEY(admin_id,campus_id),
+FOREIGN KEY (campus_id) references Campus(campus_id),
+UNIQUE (Email));
+
+create table Foods(
+food_id INT NOT NULL AUTO_INCREMENT,
+food_name VARCHAR(45),
+food_category VARCHAR(45),
+campus_id INT NOT NULL,
+PRIMARY KEY(food_id),
+FOREIGN KEY (campus_id) references Campus(campus_id),
+UNIQUE (food_name));
+
+create table Menu(
+menu_id INT NOT NULL AUTO_INCREMENT,
+menu_name VARCHAR(45),
+campus_id INT NOT NULL,
+PRIMARY KEY(menu_id),
+FOREIGN KEY (campus_id) references Campus(campus_id),
+UNIQUE (menu_name));
+
+create table Dish(
+food_id INT NOT NULL,
+menu_id INT NOT NULL,
+campus_id INT NOT NULL,
+PRIMARY KEY(food_id, menu_id),
+FOREIGN KEY (campus_id) references Campus(campus_id));
+
+create table PlanMeal(
+MDate DATE,
+Meal VARCHAR(45),
+campus_id INT NOT NULL,
+menu_id INT NOT NULL,
+PRIMARY KEY(MDate,Meal,menu_id),
+FOREIGN KEY (campus_id) references Campus(campus_id));
+
+INSERT INTO campus(campus_loc) values('STT'),('STX');
+INSERT INTO admins (FName,LName,Email,Password,user_Status,campus_id) VALUES('fish','cat','t@yahoo.com','123po','student',1);
+
+
+
+
+
+
---
+++ UVI_Dining/UVI_Dining/Controllers/ProjectController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using UVI_Dining.Models;
+
+
+namespace UVI_Dining.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+        public ActionResult login()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult login(logVal model)
+        {
+            var logVal = new logVal()
+            {
+                Email = model.Email,
+                Password = model.Password,
+
+            };
+
+
+            if (logVal.UserExists())
+            {
+                Session["FName"] = logVal.FName;
+                Session["user_Status"] = logVal.user_Status;
+                Session["campus_loc"] = logVal.campus_loc;
+                Session["campus_id"] = logVal.campus_id;
+                return View("successtest", logVal);
+            }
+            else
+            {
+                return View("login");
+            };
+
+
+        }
+        //FOR THE PROJECT 
+        public ActionResult SignUp()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        //public ActionResult SignUp(LogValidator model)
+        public ActionResult SignUp(logVal model)
+        {
+
+            var worknuh = new logVal()
+            {
+                FName = Request.Form["FName"],
+                LName = Request.Form["LName"],
+                Email = Request.Form["Email"],
+                Password = Request.Form["Password"],
+                user_Status = Request.Form["user_Status"],
+                campus_id = int.Parse(Request.Form["campus"])
+
+                //  campus_loc = Request.Form["campus_loc"]
+            };
+
+            worknuh.Admin_login();
+
+            return View();
+        }
+
+
+        public ActionResult successTest()
+        {
+          
+            return View();
+        }
+
+        public ActionResult menu()
+        {
+            return View();
+        }
+        public ActionResult contact()
+        {
+            return View();
+        }
+
+        public ActionResult aboutus()
+        {
+            return View();
+        }
+        public ActionResult location()
+        {
+            return View();
+        }
+        public ActionResult hours()
+        {
+            return View();
+        }
+        public ActionResult Foods(logVal model)
+        {
+            var l = new logVal();
+            l.campus_id = int.Parse(Session["campus_id"].ToString());
+            ViewBag.Foods = l.GetFood();
+            return View();
+        }
+
+
+
+    }
+}
